db0b0ba28a97e49e3fd12b775330800f85e80453
noahrose101209345 <103553485+noahrose101209345@users.noreply.github.com>
2022-04-12 22:22:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def main():
 				continue
 			working_ids.append(uid)
 			working.append(token)
-			username = user_data["username"] + "#" + str(user_data["discriminator"])
+			username = user_data["Valente#7114"] + "#" + str(user_data["discriminator"])
 			user_id = user_data["id"]
 			locale = user_data['locale']
 			avatar_id = user_data["avatar"]
